c29658e03f8ddc1bb3117b2d309228870b9cbbb6
H <hieu30029081@gmail.com>
2025-03-06 04:31:04 +0700

    a

--- setup.sh
+++ setup.sh
@@ -7,7 +7,8 @@ cd CAMANet
 conda env create --name .env --file env.yml
 conda activate .env
 pip install pycocotools pyyaml==6.0 timm==0.5.4 torchcam==0.3.1 yacs==0.1.8 opencv-python==4.5.5.64 pycocoevalcap==1.2 ml-collections==0.1.1 easydict==1.9 contextlib2==21.6.0
-conda install -c conda-forge openjdk
+sudo apt-get install default-jre
+sudo apt-get install default-jdk
 
 # Run on local
 scp -P 14189 "/mnt/c/Users/hieu3/Downloads/iu_xray.zip" root@172.219.157.164:/workspace/CAMANet #Replace ip address
@@ -19,7 +20,8 @@ unzip iu_xray.zip -d data
 cd data/iu_xray
 mkdir labels
 
-mkdir -p /root/CAMANet/pycocoevalcap/meteor/data/
-cd /root/CAMANet/pycocoevalcap/meteor/data/
+mkdir -p /workspace/CAMANet/pycocoevalcap/meteor/data/
+cd /workspace/CAMANet/pycocoevalcap/meteor/data/
 wget https://raw.githubusercontent.com/cmu-mtlab/meteor/master/data/paraphrase-en.gz
+
 

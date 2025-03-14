ab1622d6f66d20ce739a4ab070c32cff50244375
H <hieu30029081@gmail.com>
2025-03-14 17:04:04 +0700

    try the new pycocoeval and remove early stop

--- modules/trainer.py
+++ modules/trainer.py
@@ -121,10 +121,10 @@ class BaseTrainer(object):
                 else:
                     not_improved_count += 1
 
-                if not_improved_count > self.early_stop:
-                    self.logger.info("Validation performance didn\'t improve for {} epochs. " "Training stops.".format(
-                        self.early_stop))
-                    break
+                # if not_improved_count > self.early_stop:
+                #     self.logger.info("Validation performance didn\'t improve for {} epochs. " "Training stops.".format(
+                #         self.early_stop))
+                #     break
             self.logger.info('current best model in: {}'.format(self.best_epoch))
 
             if epoch % self.save_period == 0:
